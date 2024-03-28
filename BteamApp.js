03ca5e7572d5b559c40659ccee3ac61a386aee77
Dania Praise <praisdania@gmail.com>
2024-03-28 15:46:21 +0100

    BteamApp

--- BteamApp.js
+++ BteamApp.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyparser = require("body-parser");
 const mysql = require("mysql2");
 const cors = require("cors");
+const nodemailer = require("nodemailer");
 
 
 /********************FUNCTIONS********************/
@@ -19,16 +20,46 @@ const con = mysql.createPool({
   port: "3306",
 });
 
-/********************ENDPOINTS ********************/
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'praisdania@gmail.com',
+    pass: 'qqbqojgnvjzfqtji'
+  }
+});
+
+function generateOTP() {
+  return Math.floor(100000 + Math.random() * 500000);
+}
+
+/********************ENDPOINTS *******************/
+
+app.post("/sendOtp", bodyparser.json(), function (req, res) {
+  const { from, to, subject, text, } = req.body
+  const otp =generateOTP()
+  const message = {
+    from,
+    to,
+    subject,
+    text,
+  };
+  transporter.sendMail(message, function (error, result) {
+    if (error) { console.log(error) }
+    else { console.log(result,otp) }
+  }
+  );
+});
+
+
 
 app.post("/signup", bodyparser.json(), function (req, res) {
-  var sql = `INSERT INTO users(firstname,lastname,DateOfBirth,email,pass)
-    VALUES('${req.body.firstname}','${req.body.lastname}','${req.body.DateOfBirth}','${req.body.email}',
-    '${req.body.pass}'
-    )`;
+  var sql = `INSERT INTO users(firstname,lastname,DateOfbirth,email,pass,)
+  VALUES('${req.body.firstname}','${req.body.lastname}','${req.body.DateOfBirth}','${req.body.email}',
+  '${req.body.pass}')`;
   con.query(sql, function (err, result) {
     if (err) throw err;
     res.send(result);
+
   });
 });
 
@@ -43,7 +74,7 @@ app.get("/login", bodyparser.json(), function (req, res) {
 
 app.get("/getUser/:id", bodyparser.json(), function (req, res) {
   var sql = `SELECT * FROM users
-      WHERE id = '${req.params.id}'`;
+    WHERE user_id = '${req.params.user_id}'`;
   con.query(sql, function (err, result) {
     if (err) throw err;
     res.send(result);
@@ -52,32 +83,78 @@ app.get("/getUser/:id", bodyparser.json(), function (req, res) {
 
 
 app.post("/posts", bodyparser.json(), function (req, res) {
-  const { id, content } = req.body;
-  var sql = `INSERT INTO posts(users_id,content)
-    VALUES('${id}','${content}')`;
+  var sql = `INSERT INTO posts(user_id,content)
+    VALUES('${req.body.user_id}','${req.body.content}')`;
   con.query(sql, function (err, result) {
     if (err) throw err;
     res.send(result);
   });
 });
 
- app.post("/posts/:postid/likes", bodyparser.json(), function (req, res) {
-   const { post_id, } = req.params.post_id;
-  const { user_id, } = req.body.user_id;
-  var sql = `INSERT INTO post_likes(post_id,users_id)
-     VALUES('${post_id}','${user_id}')`;
+app.get("/posts", bodyparser.json(), function (req, res) {
+  var sql = `SELECT 
+  posts.*, 
+  COUNT(comments.comment_id) AS comment_count, 
+  COUNT(likes.like_id) AS like_count
+FROM posts
+LEFT JOIN comments ON posts.post_id = comments.post_id
+LEFT JOIN likes ON posts.post_id = likes.post_id
+GROUP BY posts.post_id; `;
   con.query(sql, function (err, result) {
     if (err) throw err;
     res.send(result);
   });
- });
+});
 
+app.post("/likes", bodyparser.json(), function (req, res) {
+  var sql = `INSERT INTO likes(user_id)
+     VALUES('${req.body.user_id}')`;
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    res.send(result);
+  });
+});
 
-app.post("/posts/:postid/comments", bodyparser.json(), function (req, res) {
-  const { post_id, } = req.params.post_id;
-  const { users_id, comment_text } = req.body.user_id;
-  var sql = `INSERT INTO post_comments(post_id,users_id)
-    VALUES('${post_id}','${users_id}','${comment_text})`;
+app.get("/alllikes", bodyparser.json(), function (req, res) {
+  var sql = `SELECT * FROM likes
+    WHERE post_id = '${req.body.post_id}'`;
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+app.delete("/deletelikes", bodyparser.json(), function (req, res) {
+  var sql = `DELETE FROM likes
+  WHERE user_id = '${req.body.user_id}'`;
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+app.post("/comments", bodyparser.json(), function (req, res) {
+  var sql = `INSERT INTO comments(user_id,content)
+    VALUES('${req.body.user_id}','${req.body.content}')`;
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+app.get("/allComments", bodyparser.json(), function (req, res) {
+  var sql = `SELECT * FROM comments
+    WHERE post_id = '${req.body.post_id}'`;
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+app.delete("/deletecomments", bodyparser.json(), function (req, res) {
+  var sql = `DELETE FROM comments
+  WHERE user_id = '${req.body.user_id}' AND content = '${req.body.content}'`
+
   con.query(sql, function (err, result) {
     if (err) throw err;
     res.send(result);
@@ -86,15 +163,26 @@ app.post("/posts/:postid/comments", bodyparser.json(), function (req, res) {
 
 
 app.post("/chats", bodyparser.json(), function (req, res) {
-  var sql = `INSERT INTO ChatMessages(sender_id,recipient_id,message)
-    VALUES('${req.body.sender_id}','${req.body.recipient_id}','${req.body.message}'
+  var sql = `INSERT INTO chats(sender_id,reciever_id,message,time_stamp)
+    VALUES('${req.body.sender_id}','${req.body.reciever_id}','${req.body.message}',
+    '${req.body.time_stamp}'
     )`;
-  con.query(sql,function (err, result) {
+  con.query(sql, function (err, result) {
     if (err) throw err;
     res.send(result);
   });
 });
 
+app.get("/chats", bodyparser.json(), function (req, res) {
+  var sql = `SELECT*FROM chats WHERE 
+    sender_id='${req.body.sender_id}' `
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+
+
+    res.send(result);
+  });
+});
 
 
 
